1338c6fb546ca5af0418a101baed2e0d83f8f7b8
Govind <Govind@GOVIND-PC>
2023-01-20 07:39:50 -0700

    Removed IsDialogMessage check from BoardEditor message loop - was preventing menu accelerators from working

--- BoardEditor/wwinmain.c
+++ BoardEditor/wwinmain.c
@@ -32,11 +32,11 @@ INT APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 
 	while (GetMessageW(&Msg, NULL, 0, 0) > 0)
 	{
-		if (!IsDialogMessageW(hWnd, &Msg))
-		{
+		//if (!IsDialogMessageW(hWnd, &Msg))
+		//{
 			TranslateMessage(&Msg);
 			DispatchMessageW(&Msg);
-		}
+		//}
 	}
 
 	return (INT) Msg.wParam;
