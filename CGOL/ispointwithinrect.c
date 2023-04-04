8be84b034c2f296007179d18d125d91cb14bacd8
Govind <Govind@GOVIND-PC>
2023-04-04 14:21:36 -0600

    doc

--- CGOL/ispointwithinrect.c
+++ CGOL/ispointwithinrect.c
@@ -1,9 +1,10 @@
 #include "CGOL.h"
 
+// Similar to "IsWithinRect", except rather than taking (x, y) coordinates it takes a `POINT p` and uses p.x and p.y
+
 _Ret_range_(FALSE, TRUE)
 
 BOOL WINAPI IsPointWithinRect(_In_ RECT rect, _In_ POINT p)
 {
 	return (((p.x >= rect.left) && (p.x <= rect.right)) && ((p.y >= rect.top) && (p.y <= rect.bottom)));
-	//return IsWithinRect(rect, p.x, p.y);
 }
